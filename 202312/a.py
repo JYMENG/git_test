2f77c6b3a57b1f65d911a6f51c7d876bbc95afa6
hao <41532093+ywluohao@users.noreply.github.com>
2023-12-09 01:20:11 -0500

    Update a.py

--- 202312/a.py
+++ 202312/a.py
@@ -1,34 +1,34 @@
 import pandas as pd
 
 # Replace the following lines with your actual DataFrame
-data = {'customer': ['c1', 'c1', 'c1'],
-        'date': ['d1', 'd2', 'd3'],
+data = {'customer_id': ['c1', 'c1', 'c1'],
+        'idp_effective_date': ['d1', 'd2', 'd3'],
         'TYPE1': [1, 1, 1],
         'TYPE2': [1, 1, 2],
         'TYPE3': [1, 2, 2]}
 
 df = pd.DataFrame(data)
 
-# Convert 'date' column to datetime format
-df['date'] = pd.to_datetime(df['date'])
+# Convert 'idp_effective_date' column to datetime format
+df['idp_effective_date'] = pd.to_datetime(df['idp_effective_date'])
 
 # Sort DataFrame by customer, type, and date
-df.sort_values(by=['customer'] + [col for col in df.columns if 'TYPE' in col] + ['date'], inplace=True)
+df.sort_values(by=['customer_id'] + df.columns[2:].tolist() + ['idp_effective_date'], inplace=True)
 
 # Identify the last score date before the change for each type
-change_dates = df[df.filter(regex='^TYPE\d').diff().ne(0).any(axis=1)].groupby(['customer']).agg(
-    **{f'{col}_last_score_date': ('date', 'first') for col in df.filter(regex='^TYPE\d').columns},
-    **{f'{col}_last_score': (col, 'last') for col in df.filter(regex='^TYPE\d').columns}
+change_dates = df[df.iloc[:, 2:].diff().ne(0).any(axis=1)].groupby(['customer_id']).agg(
+    **{f'{col}_last_score_date': ('idp_effective_date', 'first') for col in df.columns[2:]},
+    **{f'{col}_last_score': (col, 'first') for col in df.columns[2:]}
 ).reset_index()
 
 # Select the latest date and types from the last row for each customer
-result = df.groupby('customer').agg(
-    latest_date=('date', 'last'),
-    **{f'{col}': (col, 'last') for col in df.filter(regex='^TYPE\d').columns}
-).merge(change_dates, on='customer', how='left')
+result = df.groupby('customer_id').agg(
+    latest_date=('idp_effective_date', 'last'),
+    **{f'{col}': (col, 'last') for col in df.columns[2:]}
+).merge(change_dates, on='customer_id', how='left')
 
 # Update last score date and last score to blank if they are the same as the latest score
-for col in df.filter(regex='^TYPE\d').columns:
+for col in df.columns[2:]:
     if result[f'{col}_last_score'] == result[col]:
         result[f'{col}_last_score_date'] = ''
         result[f'{col}_last_score'] = ''
