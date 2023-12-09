a0c69750b33a9aa75cfe8691ecf8666f8d513bc7
hao <41532093+ywluohao@users.noreply.github.com>
2023-12-09 00:30:06 -0500

    Update a.py

--- 202312/a.py
+++ 202312/a.py
@@ -10,11 +10,15 @@ df['date'] = pd.to_datetime(df['date'])
 df.sort_values(by=['customer', 'typeA', 'typeB', 'typeC', 'date'], inplace=True)
 
 # Find the last date when the score changes for each type and customer
-last_change_dates = df[df[['typeA', 'typeB', 'typeC']].diff().ne(0).any(axis=1)].groupby(['customer']).agg(
+change_dates = df[df[['typeA', 'typeB', 'typeC']].diff().ne(0).any(axis=1)].groupby(['customer']).agg(
     typeA_last_change=('date', 'last'), 
     typeB_last_change=('date', 'last'), 
     typeC_last_change=('date', 'last')
 ).reset_index()
 
+# Merge with unique customer and type combinations to ensure all combinations are included
+result = pd.merge(pd.MultiIndex.from_product([df['customer'].unique(), df['typeA'].unique(), df['typeB'].unique(), df['typeC'].unique()], names=['customer', 'typeA', 'typeB', 'typeC']), 
+                  change_dates, how='left', on='customer')
+
 # Display the result
-print(last_change_dates)
+print(result)
