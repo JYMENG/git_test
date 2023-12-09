86ee694212bce955795607b71ed433af4af7f4ee
hao <41532093+ywluohao@users.noreply.github.com>
2023-12-09 00:24:00 -0500

    Update a.py

--- 202312/a.py
+++ 202312/a.py
@@ -1,28 +1,20 @@
 import pandas as pd
 
-# Assuming df is your DataFrame with columns ID, Date, Type A, Type B, Type C, and Value
-# Replace 'ID', 'Date', 'Type A', 'Type B', 'Type C', 'Value' with your actual column names
+# Assuming your DataFrame is named 'df'
+# Replace 'customer', 'date', 'typeA', 'typeB', 'typeC' with your actual column names
 
-# Convert the 'Date' column to datetime if it's not already
-df['Date'] = pd.to_datetime(df['Date'])
+# Convert 'date' column to datetime format
+df['date'] = pd.to_datetime(df['date'])
 
-# Find the latest date for each group (customer)
-latest_dates = df.groupby('ID')['Date'].transform('max')
+# Sort DataFrame by customer, type, and date
+df.sort_values(by=['customer', 'typeA', 'typeB', 'typeC', 'date'], inplace=True)
 
-# Filter rows to get the latest date and corresponding value for each type for each customer
-latest_values = df[df['Date'] == latest_dates]
+# Find the last date when the score changes for each type and customer
+last_change_dates = df[df[['typeA', 'typeB', 'typeC']].diff().ne(0).any(axis=1)].groupby(['customer']).agg(
+    typeA_last_change=('date', 'last'), 
+    typeB_last_change=('date', 'last'), 
+    typeC_last_change=('date', 'last')
+).reset_index()
 
-# Find the last date when the value changes for each type for each customer
-last_date_changed = pd.DataFrame()
-
-for col in ['Type A', 'Type B', 'Type C']:
-    mask = (latest_values[col] != latest_values.groupby('ID')[col].shift()) & (~latest_values[col].isna())
-    changed_values = latest_values.loc[mask, ['ID', 'Date', col]]
-    last_date_changed = pd.concat([last_date_changed, changed_values])
-
-# Display the results
-print("Latest Date and Corresponding Value for Each Type for Each Customer:")
-print(latest_values)
-
-print("\nLast Date When the Value Changes for Each Type for Each Customer:")
-print(last_date_changed)
+# Display the result
+print(last_change_dates)
