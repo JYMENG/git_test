5e06a4e349e0554613976a23bcd4ed3283ce8ed6
hao <41532093+ywluohao@users.noreply.github.com>
2024-03-17 16:53:16 -0400

    Update 0318.vue

---
+++ 202303/0318.vue
@@ -0,0 +1,28 @@
+<template>
+  <!-- Your Vue.js component template -->
+  <button @click="generateExcel">Generate Excel</button>
+</template>
+
+<script>
+export default {
+  methods: {
+    generateExcel() {
+      // Assuming you have your JSON object stored in the data property named jsonData
+      fetch('http://your-django-backend-url/generate_excel/', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(this.jsonData)
+      })
+      .then(response => {
+        // Use window.location.href to trigger the download
+        window.location.href = response.url;
+      })
+      .catch(error => {
+        console.error('Error generating Excel:', error);
+      });
+    }
+  }
+}
+</script>
